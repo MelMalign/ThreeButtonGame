a1aaa207c5ba38a2a1be369eddcbfe14fec5b282
Jet Yong <jettlag2000@gmail.com>
2020-07-03 13:58:44 +1200

    ResetOnDeath

--- Assets/Scripts/DeathTrap.cs
+++ Assets/Scripts/DeathTrap.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathTrap : MonoBehaviour
 {
+    public float resetDelay = 1f;
 
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Player")
+        {
             Destroy(col.gameObject);
+            Time.timeScale = .5f;
+            Invoke("Reset", resetDelay);
+        }
+    }
+
+    private void Reset()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
